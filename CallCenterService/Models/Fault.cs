83da44521aa170e9005e7111da504b731f927a7f
unknown <kuba.jjjankowski@gmail.com>
2018-04-14 22:43:40 +0200

    Adding Fault model

---
+++ CallCenterService/Models/Fault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallCenterService.Models
+{
+    public class Fault
+    {
+        public int Id { get; set; }
+        public String ClientFirstName { get; set; }
+        public String ClientSecondName { get; set; }
+        public int ClientId { get; set; }
+        public String Description { get; set; }
+        public String PaymentData { get; set; }
+    }
+}
