5c160dc0134d3fcb39ed1cc4addefbc574947326
Damian Smarkucki <damiansmarkucki@gmail.com>
2018-05-03 13:36:12 +0200

    set servicer duplicate fix + servicer data in registrant panel

--- CallCenterService/Controllers/RegistrantController.cs
+++ CallCenterService/Controllers/RegistrantController.cs
@@ -18,9 +18,41 @@ namespace CallCenterService.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Faults.ToListAsync());
+            var vm = new List<IndexRegistrantViewModel>();
+
+            var faults = _context.Faults.ToList();
+            foreach(var fault in faults)
+            {
+                var ivm = new IndexRegistrantViewModel();
+                ivm.FaultData = fault;
+
+                var sf = _context.ServicerFault.FirstOrDefault(f => f.IdFault == fault.FaultId);
+                if(sf == null)
+                {
+                    ivm.Servicer = new Servicer();
+                    ivm.Servicer.FirstName = "";
+                    ivm.Servicer.SecondName = "";
+                    ivm.Servicer.Specialization = "";
+                }
+                else
+                {
+                    var servicer = _context.Servicers.FirstOrDefault(f => f.ServicerId == sf.IdServicer);
+                    if (servicer == null)
+                    {
+                        ivm.Servicer = new Servicer();
+                        ivm.Servicer.FirstName = "";
+                        ivm.Servicer.SecondName = "";
+                        ivm.Servicer.Specialization = "";
+                    }
+                    else
+                        ivm.Servicer = servicer;
+                }
+                vm.Add(ivm);
+            }
+
+            return View(vm);
         }
 
         public IActionResult AddFault()
@@ -74,6 +106,8 @@ namespace CallCenterService.Controllers
                     IdFault = vm.FaultId,
                     IdServicer = vm.ServicerId
                 };
+
+                _context.ServicerFault.RemoveRange(_context.ServicerFault.Where(x => x.IdFault == vm.FaultId));
 
                 await _context.ServicerFault.AddAsync(sf);
                 await _context.SaveChangesAsync();
---
+++ CallCenterService/ViewModels/IndexRegistrantViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CallCenterService.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CallCenterService.ViewModels
+{
+    public class IndexRegistrantViewModel
+    {
+        public Fault FaultData { get; set; }
+        public Servicer Servicer { get; set; }
+    }
+}
