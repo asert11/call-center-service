1c31f38baa41901588c142cab53d90a0a5ac5c71
Damian Smarkucki <damiansmarkucki@gmail.com>
2018-06-04 15:58:09 +0200

    spelling correction :p

--- CallCenterService/ViewModels/LoginViewModel.cs
+++ CallCenterService/ViewModels/LoginViewModel.cs
@@ -8,7 +8,7 @@ namespace CallCenterService.ViewModels
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Pole hasło jest wymagane"), 
-            MinLength(6, ErrorMessage = "Hasło powinno zawierać conajmniej 6 znaków"), 
+            MinLength(6, ErrorMessage = "Hasło powinno zawierać co najmniej 6 znaków"), 
             MaxLength(50), DataType(DataType.Password), Display(Name = "Password")]
         public string Password { get; set; }
 
