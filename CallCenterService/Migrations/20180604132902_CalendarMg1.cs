63de49bbe41165f58eb765cbed399141844f1b0a
Kuba <kuba.jjjankowski@gmail.com>
2018-06-06 14:17:01 +0200

    Initial commit

---
+++ CallCenterService/Migrations/20180604132902_CalendarMg1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CallCenterService.Migrations
+{
+    public partial class CalendarMg1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
