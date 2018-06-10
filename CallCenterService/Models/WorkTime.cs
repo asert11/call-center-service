840e5f241daab474322f2793bbc4a488cb691b36
Patryś <patryk.chrapusta@interia.pl>
2018-06-10 19:51:18 +0200

    fix validation

--- CallCenterService/Models/WorkTime.cs
+++ CallCenterService/Models/WorkTime.cs
@@ -10,47 +10,47 @@ namespace CallCenterService.Models
     {
         public int WorkTimeId { get; set; }
         public String ServicerId { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")] 
+       
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")] 
         public String MondayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String MondayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String TuesdayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String TuesdayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String WednesdayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String WednesdayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String ThursdayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String ThursdayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String FridayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String FridayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String SaturdayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String SaturdayEnd { get; set; }
-        [Required]
-        [RegularExpression("^[0-9]$|^[0-1][0-9]$|^2[1-3]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String SundayStart { get; set; }
-        [Required]
-        [RegularExpression("^[0-5][0-9]$|^[0-9]$", ErrorMessage = "Niepoprawne dane")]
+
+        [RegularExpression("^[0-9]:[0-5][0-9]$|^[0-1][0-9]:[0-5][0-9]$|^2[1-3]:[0-5][0-9]$", ErrorMessage = "Niepoprawne dane")]
         public String SundayEnd { get; set; }
     }
 }
